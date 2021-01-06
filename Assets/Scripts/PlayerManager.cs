a6186fad262f5e0ca0b3579babf79a40f77e3fb8
Achal2K <58651687+MichalPetricek123@users.noreply.github.com>
2021-01-06 10:49:15 +0100

    .

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject dedScreen;
+    public bool deadMan = false;
+
+    void Update()
+    {
+        if (health < 0) { dead(); }
+    }
+    void dead()
+    {
+        dedScreen.SetActive(true);
+        deadMan = true;
+    }
+}
---
+++ Assets/Scripts/dedscript.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dedscript : MonoBehaviour
+{
+    public GameObject deadScreenUIObject;
+}
